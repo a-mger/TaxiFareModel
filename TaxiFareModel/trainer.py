1a9f72028d76b89707f34c31e49472f04389604b
Andreas Mussger <andreas.mussger@gmail.com>
2021-06-01 15:43:50 +0200

    corrected encoder

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,9 +1,11 @@
-# imports
-from sklearn.linear_model import LinearRegression
+from TaxiFareModel.data import get_data, clean_data
+from TaxiFareModel.encoders import TimeFeaturesEncoder, DistanceTransformer
+from TaxiFareModel.utils import compute_rmse
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+import pandas as pd
 
 
 class Trainer():
@@ -16,40 +18,44 @@ class Trainer():
         self.X = X
         self.y = y
 
-
-    def set_pipeline():
-        '''returns a pipelined model'''
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
         dist_pipe = Pipeline([('dist_trans', DistanceTransformer()),
-                            ('stdscaler', StandardScaler())])
-        time_pipe = Pipeline([('time_enc', TimeFeaturesEncoder('pickup_datetime')),
-                            ('ohe', OneHotEncoder(handle_unknown='ignore'))])
+                              ('stdscaler', StandardScaler())])
+        time_pipe = Pipeline([('time_enc',
+                               TimeFeaturesEncoder('pickup_datetime')),
+                              ('ohe', OneHotEncoder(handle_unknown='ignore'))])
         preproc_pipe = ColumnTransformer([('distance', dist_pipe, [
             "pickup_latitude", "pickup_longitude", 'dropoff_latitude',
             'dropoff_longitude'
         ]), ('time', time_pipe, ['pickup_datetime'])],
-                                        remainder="drop")
-        pipe = Pipeline([('preproc', preproc_pipe),
-                        ('linear_model', LinearRegression())])
-        return pipe
+                                         remainder="drop")
+
+        self.pipeline = Pipeline([('preproc', preproc_pipe),
+                                  ('linear_model', LinearRegression())])
 
     def run(self):
         """set and train the pipeline"""
-        self.pipeline = set_pipeline()
+        self.set_pipeline()
         self.pipeline.fit(self.X, self.y)
-        return self.pipeline
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
-        rmse = np.sqrt(((y_pred - y_test)**2).mean())
-        return rmse
+        rmse = compute_rmse(y_pred, y_test)
+        return round(rmse, 2)
 
 
 if __name__ == "__main__":
-    # get_data
-    # clean data
-    # set X and y
-    # hold out
-    # train
-    # evaluate
-    print(df.head())
+    N = 10_000
+    df = get_data(nrows=N)
+    df = clean_data(df)
+    y = df["fare_amount"]
+    X = df.drop("fare_amount", axis=1)
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    trainer = Trainer(X_train, y_train)
+    trainer.run()
+    rmse = trainer.evaluate(X_test, y_test)
+    print(f"rmse: {rmse}")
